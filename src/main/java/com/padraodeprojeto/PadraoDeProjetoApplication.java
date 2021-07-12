1f545e9c0e56213bb3fa11c66c4049b5c038536f
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2021-07-11 21:03:01 -0300

    Conexão com Github

---
+++ src/main/java/com/padraodeprojeto/PadraoDeProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.padraodeprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadraoDeProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadraoDeProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/padraodeprojeto/PadraoDeProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padraodeprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadraoDeProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
